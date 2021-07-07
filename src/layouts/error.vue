dd56b90360aa7ae60d53c83be576a18348e766ee
Manuel Odelain <manuel@rezo-zero.com>
2021-07-07 18:20:54 +0200

    error layout: fix TS error

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <h1>{{ title }}</h1>
+        <h1>{{ $t(error.statusCode === 404 ? 'page_not_found' : 'page_error') }}</h1>
         <a :href="linkHref">{{ $t('back_home') }}</a>
     </section>
 </template>
@@ -16,9 +16,6 @@ export default Vue.extend({
         error: Object as PropType<NuxtError>,
     },
     computed: {
-        title(): string {
-            return this.$t(this.error.statusCode === 404 ? 'page_not_found' : 'page_error')
-        },
         linkHref(): string {
             return this.$store.state.headData?.homePageUrl || '/'
         },
