df66b2275c622bfa07416c6720cb966283071792
Manuel Odelain <manuel@rezo-zero.com>
2021-07-07 17:26:42 +0200

    setup stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
-  ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-idiomatic-order',
+        'stylelint-config-css-modules',
+        'stylelint-config-prettier'
+    ],
+    'plugins': [
+        'stylelint-scss',
+        'stylelint-order'
+    ],
+    // add your custom config here
+    // https://stylelint.io/user-guide/configuration
+    rules: {
+        'no-descending-specificity': null,
+        'indentation': 4,
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true
+    },
+    'defaultSeverity': 'warning'
 }
