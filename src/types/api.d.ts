71ac7b04995e76c370bdf8fffc4470a20728368a
Manuel Odelain <manuel@rezo-zero.com>
2021-07-08 11:25:39 +0200

    api: remove unused TS declarations

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,28 +1,7 @@
-import RoadizApi from "@roadiz/abstract-api-client";
-import {RoadizNodesSources} from "@roadiz/abstract-api-client/dist/types/roadiz";
-import {AlternateLink} from "@roadiz/abstract-api-client/dist/types/roadiz-api";
+import { RoadizNodesSources } from '@roadiz/abstract-api-client/dist/types/roadiz'
+import { AlternateLink } from '@roadiz/abstract-api-client/dist/types/roadiz-api'
 
 interface PageResponse {
     page: RoadizNodesSources
     alternateUrls?: AlternateLink[]
-}
-
-declare module '@nuxt/types' {
-    interface Context {
-        $api: RoadizApi
-    }
-
-    interface NuxtAppOptions {
-        $api: RoadizApi
-    }
-
-    interface Configuration {
-        $api?: RoadizApi
-    }
-}
-
-declare module 'vue/types/vue' {
-    interface Vue {
-        $api: RoadizApi
-    }
 }
