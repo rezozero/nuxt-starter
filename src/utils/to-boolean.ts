0ae391253bba58c738fa39630ada4dca2a7cfcea
Manuel Odelain <manuel@rezo-zero.com>
2021-08-16 15:51:06 +0200

    improve toBoolean method

--- src/utils/to-boolean.ts
+++ src/utils/to-boolean.ts
@@ -1,6 +1,3 @@
-export default function toBoolean(value: boolean | string | number | undefined): boolean {
-    return (
-        typeof value !== 'undefined' &&
-        (value === true || value === 'true' || value === 'True' || value === 'TRUE' || value === 1)
-    )
+export default function toBoolean(value: any): boolean {
+    return typeof value === 'string' ? (value as string).toLowerCase() === 'true' : !!value
 }
