8e344477b27e4644fe52244e56061f64aa0566aa
Manuel Odelain <manuel@rezo-zero.com>
2022-01-28 14:17:17 +0100

    update BlockFactory

--- src/components/organisms/BlockFactory.vue
+++ src/components/organisms/BlockFactory.vue
@@ -1,50 +1,46 @@
 <script lang="ts">
 import Vue, { PropType, VNode } from 'vue'
 import { RoadizWalker } from '@roadiz/abstract-api-client/dist/types/roadiz'
+import EventType from '~/constants/event-type'
 
 export default Vue.extend({
     name: 'BlockFactory',
     functional: true,
     props: {
         blocks: {
-            type: Array as PropType<RoadizWalker[]>,
+            type: Array as PropType<Array<RoadizWalker>>,
             default: () => [],
         },
     },
-    render(createElement, context): VNode {
-        return createElement(
-            // TODO: use directly an array of VNode
-            'div',
-            {
-                attrs: { class: 'block-factory' },
-            },
-            context.props.blocks
-                .filter(
-                    (block: RoadizWalker) =>
-                        context.parent.$root.$options.components &&
-                        block.item['@type'] in context.parent.$root.$options.components
-                )
-                .map((block, index, blocks) => {
-                    return createElement(block.item['@type'], {
-                        props: {
-                            walker: block,
-                            index,
-                            numBlocks: blocks.length,
+    render(createElement, context): VNode[] | VNode {
+        if (!context.props.blocks.length) return createElement('div') // fix DOM mismatch without blocks
+
+        return context.props.blocks
+            .filter(
+                (block: RoadizWalker) =>
+                    context.parent.$root.$options.components &&
+                    block.item['@type'] in context.parent.$root.$options.components
+            )
+            .map((block, index, blocks) => {
+                return createElement(block.item['@type'], {
+                    props: {
+                        walker: block,
+                        index,
+                        numBlocks: blocks.length,
+                    },
+                    attrs: {
+                        id: block.item.node.nodeName,
+                    },
+                    on: {
+                        [EventType.BLOCK_MOUNTED]() {
+                            // TODO: transform the event name to pascal case automatically
+                            const callback = context.listeners['block-mounted']
+
+                            if (typeof callback === 'function') callback()
                         },
-                        attrs: {
-                            id: block.item.node.nodeName,
-                        },
-                        // on: {
-                        //     [EventType.BLOCK_MOUNTED]() {
-                        //         // TODO: transform the event name to pascal case automatically
-                        //         const callback = context.listeners['block-mounted']
-                        //
-                        //         if (typeof callback === 'function') callback()
-                        //     },
-                        // },
-                    })
+                    },
                 })
-        )
+            })
     },
 })
 </script>
