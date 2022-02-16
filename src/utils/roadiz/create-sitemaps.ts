c8de899da2905ef8e730042bfd9a2403defc15d0
Manuel Odelain <manuel@rezo-zero.com>
2022-02-16 18:20:15 +0100

    fix: sitemap during build on CI

--- src/utils/roadiz/create-sitemaps.ts
+++ src/utils/roadiz/create-sitemaps.ts
@@ -3,24 +3,29 @@ import { joinURL } from 'ufo'
 import toBoolean from '../to-boolean'
 
 export default function createSitemaps(locales: Array<string>) {
-    if (!process.env.API_URL) {
-        throw new Error('API URL is not configured.')
-    }
-
-    if (!locales || locales.length < 1) {
-        throw new Error('No available locale is configured.')
-    }
-
-    const $roadiz = new RoadizApi(joinURL(process.env.API_URL || '', process.env.API_ENDPOINT_PREFIX || ''), {
-        apiKey: process.env.API_KEY,
-        preview: toBoolean(process.env.API_PREVIEW),
-        debug: toBoolean(process.env.API_DEBUG),
-    })
-
     return locales.map((locale: string) => {
         return {
             path: `/sitemap/sitemap-${locale}.xml`,
-            routes: async () => await $roadiz.fetchAllUrlsForLocale(locale),
+            routes() {
+                if (!process.env.API_URL) {
+                    throw new Error('API URL is not configured.')
+                }
+
+                if (!locales || locales.length < 1) {
+                    throw new Error('No available locale is configured.')
+                }
+
+                const $roadiz = new RoadizApi(
+                    joinURL(process.env.API_URL || '', process.env.API_ENDPOINT_PREFIX || ''),
+                    {
+                        apiKey: process.env.API_KEY,
+                        preview: toBoolean(process.env.API_PREVIEW),
+                        debug: toBoolean(process.env.API_DEBUG),
+                    }
+                )
+
+                return $roadiz.fetchAllUrlsForLocale(locale)
+            },
         }
     })
 }
