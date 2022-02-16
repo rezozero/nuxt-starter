0beeb8067cc66caca72ccb72356acbc12265b48c
Manuel Odelain <manuel@rezo-zero.com>
2022-02-16 17:45:11 +0100

    fix: generate sitemap

--- src/utils/roadiz/create-sitemaps.ts
+++ src/utils/roadiz/create-sitemaps.ts
@@ -1,4 +1,5 @@
 import RoadizApi from '@roadiz/abstract-api-client'
+import { joinURL } from 'ufo'
 import toBoolean from '../to-boolean'
 
 export default function createSitemaps(locales: Array<string>) {
@@ -10,7 +11,7 @@ export default function createSitemaps(locales: Array<string>) {
         throw new Error('No available locale is configured.')
     }
 
-    const $roadiz = new RoadizApi(process.env.API_URL, {
+    const $roadiz = new RoadizApi(joinURL(process.env.API_URL || '', process.env.API_ENDPOINT_PREFIX || ''), {
         apiKey: process.env.API_KEY,
         preview: toBoolean(process.env.API_PREVIEW),
         debug: toBoolean(process.env.API_DEBUG),
