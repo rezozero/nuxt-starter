aee7c962750524c0fef66f602174e53e9d68112b
Ambroise Maupate <ambroise@rezo-zero.com>
2022-05-05 16:49:43 +0200

    feat: Do not cache nor store previewed responses

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,8 +1,17 @@
 import { ServerMiddleware } from '@nuxt/types'
+import toBoolean from '../utils/to-boolean'
 
-const cacheMiddleware: ServerMiddleware = function (req, res, next) {
-    // Adds a minimum 30 seconds for Varnish to cache Nuxt responses.
-    res.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate')
+const cacheMiddleware: ServerMiddleware = function (_req, res, next) {
+    const allowClientPreview = toBoolean(process.env.API_ALLOW_CLIENT_PREVIEW)
+    const searchParams = new URL(`http://localhost` + _req.url).searchParams
+
+    if (allowClientPreview && searchParams.has('token') && searchParams.get('_preview') === '1') {
+        // Do not cache nor store previewed responses
+        res.setHeader('Cache-Control', 'no-cache, no-store')
+    } else {
+        // Adds a minimum 30 seconds for Varnish to cache Nuxt responses.
+        res.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate')
+    }
     next()
 }
 
