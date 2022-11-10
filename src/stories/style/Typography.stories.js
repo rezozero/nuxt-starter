1defedae94ccde0f62c541e704bd022466bf0628
Manuel Odelain <manuel@rezo-zero.com>
2022-11-10 14:06:57 +0100

    feat: finish base style + add stories

---
+++ src/stories/style/Typography.stories.js
@@ -0,0 +1,36 @@
+export default {
+    title: 'Style/Typography',
+}
+
+export const h1 = () => ({
+    template: '<h1 class="text-h1">Lorem ipsum dolor sit amet</h1>',
+})
+
+export const h2 = () => ({
+    template: '<h2 class="text-h2">Lorem ipsum dolor sit amet consectetur adipiscing</h2>',
+})
+
+export const h3 = () => ({
+    template:
+        '<h3 class="text-h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</h3>',
+})
+
+export const h4 = () => ({
+    template:
+        '<h4 class="text-h4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</h4>',
+})
+
+export const h5 = () => ({
+    template:
+        '<h5 class="text-h5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</h5>',
+})
+
+export const body = () => ({
+    template:
+        '<div class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>',
+})
+
+export const bodyS = () => ({
+    template:
+        '<div class="text-body-s">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>',
+})
