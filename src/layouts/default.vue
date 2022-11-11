da12c072aad1c32f293d03f3edcf3581b10bceee
Manuel Odelain <manuel@rezo-zero.com>
2022-11-11 18:38:59 +0100

    feat(layout): remove track page view

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,16 +7,15 @@
 <script lang="ts">
 import mixins from 'vue-typed-mixins'
 import MutationType from '../constants/mutation-type'
-import { trackPageView } from '~/tracking/tracker'
 import Resize from '~/mixins/Resize'
 
 export default mixins(Resize).extend({
-    watch: {
-        $route() {
-            // TODO: we have to track page view in another place if we want to have the page title
-            trackPageView(undefined, this.$route.fullPath)
-        },
-    },
+    // watch: {
+    //     $route() {
+    //         // TODO: we have to track page view in another place if we want to have the page title
+    //         trackPageView(undefined, this.$route.fullPath)
+    //     },
+    // },
     mounted() {
         this.$store.commit(
             MutationType.PREFERS_REDUCED_MOTION,
