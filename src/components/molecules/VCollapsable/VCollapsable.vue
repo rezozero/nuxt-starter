996a21faff7d401dc4050546afb905ea0469a5f7
Manuel Odelain <manuel@rezo-zero.com>
2022-11-13 21:37:07 +0100

    feat: add VCollapsable

---
+++ src/components/molecules/VCollapsable/VCollapsable.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import Vue from 'vue'
+import type { VNode } from 'vue'
+
+export default Vue.extend({
+    name: 'VCollapsable',
+    props: {
+        enabled: Boolean,
+        value: Boolean, // collapsed
+    },
+    methods: {
+        toggle() {
+            this.$emit('input', !this.value)
+        },
+    },
+    render(createElement): VNode {
+        const scopedSlot = this.$scopedSlots.default?.({
+            toggle: this.toggle,
+        })?.[0]
+
+        return scopedSlot || createElement('')
+    },
+})
+</script>
