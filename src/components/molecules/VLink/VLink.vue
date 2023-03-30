e45e174730c9c0f691b388beccecadcc65ca299c
Manuel Odelain <manuel@rezo-zero.com>
2023-03-30 10:16:44 +0200

    fix(VLink): href attribute for NUxtLink

--- src/components/molecules/VLink/VLink.vue
+++ src/components/molecules/VLink/VLink.vue
@@ -6,7 +6,9 @@ import VueI18n from 'vue-i18n'
 import { RenderContext } from 'vue/types/options'
 import { EventsApi } from '~/types/event'
 
-type Reference = RoadizNodesSources | EventsApi.Event
+export type Reference = RoadizNodesSources | EventsApi.Event
+
+type CustomVNodeData = VNodeData & Required<Pick<VNodeData, 'props' | 'attrs'>>
 
 function getDefaultLabel(context: RenderContext, label: string): VueI18n.TranslateResult | false {
     return context.parent.$te(label) && context.parent.$t(label)
@@ -42,20 +44,12 @@ export default Vue.extend({
             )
         }
 
-        const data: VNodeData = Object.assign({}, context.data)
-        const isInternal = !!reference || url?.charAt(0) === '/' || url?.charAt(0) === '#'
+        const data: CustomVNodeData = Object.assign({ props: {}, attrs: {} }, context.data)
+        const isRelative = url?.charAt(0) === '/'
+        const isInternal = (!!reference || isRelative || url?.charAt(0) === '#') && !context.data.attrs?.target
         const isExternal = !isInternal && !!url
         const isDownload = !isInternal && !isExternal && !!document
-
-        if (!data.props) data.props = {}
-        if (!data.attrs) data.attrs = {}
-
         let label: string | VueI18n.TranslateResult | undefined | boolean = context.props.label
-        let href!: string
-        let target!: string
-        let rel!: string
-        let to!: string
-        let staticClass!: string
 
         if (!label && label !== false) {
             if (isExternal) {
@@ -72,26 +66,31 @@ export default Vue.extend({
         }
 
         if (isDownload) {
-            href = context.parent.$documentURL(document.relativePath)
-            target = '_blank'
-            rel = 'noopener'
-            // staticClass = 'matomo_download'
+            data.attrs = {
+                ...data.attrs,
+                href: context.parent.$documentURL(document.relativePath),
+                target: '_blank',
+                rel: 'noopener',
+            }
         } else if (isInternal) {
-            to = reference?.url || url
+            data.props.to = reference?.url || url
         } else if (isExternal) {
-            href = url
-            target = '_blank'
-            rel = 'noopener'
+            data.attrs = {
+                ...data.attrs,
+                href: url,
+                target: context.data.attrs?.target || '_blank',
+                rel: !isRelative && 'noopener',
+            }
         }
 
         if (context.props.custom) {
             const vNodes = context.scopedSlots.default?.({
                 label,
-                href,
-                target,
-                rel,
-                to,
-                staticClass,
+                href: data.attrs.href,
+                target: data.attrs.target,
+                rel: data.attrs.target,
+                to: data.props.to,
+                staticClass: data.staticClass,
             })
 
             if (vNodes && !vNodes?.[0]?.text) {
@@ -99,25 +98,13 @@ export default Vue.extend({
             }
         }
 
-        if (!href && !to) {
+        if (!data.attrs!.href && !data.props!.to) {
             return context.slots()?.default || (label ? createElement('span', String(label)) : createElement(''))
         }
 
         return createElement(
             isExternal || isDownload ? 'a' : 'nuxt-link',
-            {
-                ...context.data,
-                staticClass,
-                attrs: {
-                    ...context.data.attrs,
-                    rel,
-                    href,
-                    target,
-                },
-                props: {
-                    to,
-                },
-            },
+            data,
             context.slots()?.default || (label && String(label)) || ''
         )
     },
