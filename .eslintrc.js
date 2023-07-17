49b3d6dc798f8ce87a7d1aa496ae5c47b9e86d61
Manuel Odelain <manuel@rezo-zero.com>
2023-07-17 16:01:49 +0200

    build: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,15 @@ module.exports = {
     },
     extends: ['@nuxtjs/eslint-config-typescript', 'prettier', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
     plugins: ['prettier'],
-    // add your custom rules here
     rules: {
         // deactivate temp because of this issue https://github.com/typescript-eslint/typescript-eslint/issues/2477
         'no-undef': 'off',
         'no-use-before-define': 'off',
         'vue/require-default-prop': 'off',
+        // We possibly use CommonJS import
+        'no-var-requires': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        // TypeScript compilation already ensures that named imports exist in the referenced module
+        'import/named': 'off',
     },
 }
