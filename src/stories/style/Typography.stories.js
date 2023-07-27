4d7007fca3eacdc7fe05aa5d23eecb831c359037
timothe <timothe@rezo-zero.com>
2023-07-27 15:32:47 +0200

    feat: add usefull scss font mixin

--- src/stories/style/Typography.stories.js
+++ src/stories/style/Typography.stories.js
@@ -2,6 +2,24 @@ export default {
     title: 'Style/Typography',
 }
 
+export const All = () => ({
+    template: `
+    <div>
+        <h1 class="text-h1">Lorem ipsum dolor sit amet</h1>
+        <h2 class="text-h2">Lorem ipsum dolor sit amet consectetur adipiscing</h2>
+        <h3 class="text-h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</h3>
+        <h4 class="text-h4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</h4>
+        <h5 class="text-h5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</h5>
+        <div class="text-overtitle-m">text-overtitle-m</div>
+        <div class="text-overtitle-s">text-overtitle-s</div>
+        <div class="text-body-l">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+        <div class="text-body-m">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+        <div class="text-body-s">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+        <div class="text-body-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+    </div>
+    `,
+})
+
 export const h1 = () => ({
     template: '<h1 class="text-h1">Lorem ipsum dolor sit amet</h1>',
 })
