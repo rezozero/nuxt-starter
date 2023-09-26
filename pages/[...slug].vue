4cfa3f6339ec3867b9093b6c90987af3ebdf4d1e
Manuel Odelain <manuel@rezo-zero.com>
2023-09-26 10:28:24 +0200

    chore: remove example from main page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-import IconCheck from '~/assets/images/icons/check.svg?component'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>
-        <SvgIcon name="check" width="14" height="11" />
-        <IconCheck />
-    </div>
+    <div></div>
 </template>
 
 <style module lang="scss"></style>
