46d7de561056f8b05765487adbc50b932d6a3750
Manuel Odelain <manuel@rezo-zero.com>
2024-03-01 13:51:02 +0100

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { NuxtError } from 'nuxt/app'
+
+defineProps<{
+    error: NuxtError
+}>()
+</script>
+
+<template>
+    <div>
+        <div v-if="error.statusCode === 404">
+            {{ $t('error.not_found.title') }}
+        </div>
+        <div v-else>
+            {{ $t('error.title') }}
+        </div>
+        <div v-if="error.statusCode === 404">
+            {{ $t('error.not_found.body') }}
+        </div>
+        <div v-else>
+            {{ $t('error.body') }}
+        </div>
+        <a href="/">
+            {{ $t('error.back_home') }}
+        </a>
+    </div>
+</template>
+
+<style lang="scss" module></style>
