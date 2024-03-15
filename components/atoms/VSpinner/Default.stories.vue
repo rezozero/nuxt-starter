1b8119feab7bf21f4442ea04b97131c29d364a9a
Manuel Odelain <manuel@rezo-zero.com>
2024-03-15 16:39:27 +0100

    feat: add VSpinner

---
+++ components/atoms/VSpinner/Default.stories.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const playState = ref('running')
+
+function toggle() {
+    playState.value = playState.value === 'running' ? 'paused' : 'running'
+}
+</script>
+
+<template>
+    <NuxtStory>
+        <template #aside>
+            <VButton filled @click="toggle">Toggle state</VButton>
+        </template>
+        <VSpinner :play-state="playState" :class="$style.root" />
+    </NuxtStory>
+</template>
+
+<style lang="scss" module>
+.root {
+    width: 200px;
+    height: 200px;
+}
+</style>
---
+++ components/atoms/VSpinner/VSpinner.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        playState?: 'paused' | 'running'
+    }>(),
+    {
+        playState: 'running',
+    },
+)
+</script>
+
+<template>
+    <svg :class="$style.root" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+        <circle :class="$style.circle" cx="25" cy="25" r="20" stroke-width="5" />
+    </svg>
+</template>
+
+<style lang="scss" module>
+// @see https://codepen.io/zapdev/pen/MYJRaj
+
+@keyframes rotate {
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+@keyframes dash {
+    0% {
+        stroke-dasharray: 1, 200;
+        stroke-dashoffset: 0;
+    }
+
+    50% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -35;
+    }
+
+    100% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -124;
+    }
+}
+
+.root {
+    animation: rotate 1.5s linear infinite;
+    animation-play-state: var(--v-spinner-play-state, v-bind(playState));
+}
+
+.circle {
+    animation: dash 1.5s ease-in-out infinite 0s;
+    animation-play-state: inherit;
+    fill: none;
+    stroke: currentcolor;
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+    stroke-linecap: round;
+}
+</style>
