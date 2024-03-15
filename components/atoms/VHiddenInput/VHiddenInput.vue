4baf78037d07c3c178b4de2860d42f66913e13fb
Manuel Odelain <manuel@rezo-zero.com>
2024-03-15 16:38:31 +0100

    feat: add VHiddenInput

---
+++ components/atoms/VHiddenInput/VHiddenInput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { FormElementProps } from '~/types/form'
+
+defineProps<FormElementProps>()
+</script>
+
+<template>
+    <input :id="id" type="hidden" :name="name" :value="modelValue" />
+</template>
