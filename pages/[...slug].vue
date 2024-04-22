02c4db2f4e93d8211369370fd137795ab46faede
Manuel Odelain <manuel@rezo-zero.com>
2024-04-22 17:07:25 +0200

    feat: Force redirect when web response URL is not matching current route path

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,4 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// definePageMeta({
+//     pageTransition: defaultPageTransition,
+// })
+
+import type { RoadizNodesSources } from '@roadiz/types'
+
+const { item, error } = await useRoadizWebResponse<RoadizNodesSources>()
+
+if (error) {
+    showError(error)
+}
+
+const route = useRoute()
+// Force redirect when web response URL is not matching current route path
+if (item?.url && item.url !== route.path) {
+    await navigateTo({ path: item?.url }, { redirectCode: 301 })
+}
+</script>
 
 <template>
     <div>Hello world</div>
