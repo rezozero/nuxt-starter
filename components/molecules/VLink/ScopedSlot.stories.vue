d51d24a27728237f3ebf7c11b4bcbf4858773ce4
timothe <timothe@rezo-zero.com>
2024-06-11 16:33:23 +0200

    chore(VLink): adapt stories

--- components/molecules/VLink/ScopedSlot.stories.vue
+++ components/molecules/VLink/ScopedSlot.stories.vue
@@ -9,13 +9,13 @@
         </NuxtStoryVariant>
         <NuxtStoryVariant title="External url">
             <VLink v-slot="linkProps" url="https://google.com" custom>
-                <VButton label="Button Slot" v-bind="linkProps" filled theme="light" />
+                <VButton label="Bind all scoped slot props" v-bind="linkProps" />
+            </VLink>
+            <VLink v-slot="{ href, target, rel }" url="https://google.com" custom>
+                <VButton :href="href" :target="target" :rel="rel" label="Scoped slot props destructuring" />
             </VLink>
         </NuxtStoryVariant>
         <NuxtStoryVariant title="External url">
-            <VLink v-slot="{ href, target, rel }" url="https://google.com" custom>
-                <VButton :href="href" :target="target" :rel="rel" filled icon-name="check" label="Label" />
-            </VLink>
             <VLink v-slot="{ href, target, rel }" url="https://google.com" custom>
                 <VButton :href="href" :target="target" :rel="rel" theme="dark" filled icon-name="check" label="Label" />
             </VLink>
