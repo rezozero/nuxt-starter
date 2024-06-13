ec3ffdad27877bc5574cce8e27280e96e2b6eb7a
Manuel Odelain <manuel@rezo-zero.com>
2024-06-13 14:50:24 +0200

    feat: add VCarousel

---
+++ components/organisms/VCarousel/AsyncSlides.stories.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import VStoryAsyncSlide from './VStoryAsyncSlide.vue'
+
+const slideIndex = ref(0)
+const numSlides = 10
+const snapGridLength = ref(numSlides)
+</script>
+
+<template>
+    <NuxtStory>
+        <div
+            style="width: 500px; max-width: 100vw; border: 1px solid #ccc; overflow: hidden"
+        >
+            <VCarousel
+                v-slot="{ slideClass }"
+                v-model="slideIndex"
+                @snap-grid-length-change="snapGridLength = $event"
+            >
+                <VStoryAsyncSlide
+                    v-for="i in numSlides"
+                    :key="i"
+                    :class="[...slideClass]"
+                    :index="i"
+                />
+            </VCarousel>
+            <VCarouselControls
+                v-model:index="slideIndex"
+                :length="snapGridLength"
+            />
+        </div>
+    </NuxtStory>
+</template>
---
+++ components/organisms/VCarousel/Default.stories.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const slideIndex = ref(0)
+const numSlides = 10
+const snapGridLength = ref(numSlides)
+</script>
+
+<template>
+    <NuxtStory>
+        <div
+            style="width: 500px; max-width: 100vw; border: 1px solid #ccc; overflow: hidden"
+        >
+            <VCarousel
+                v-slot="{ slideClass }"
+                v-model="slideIndex"
+                @snap-grid-length-change="snapGridLength = $event"
+            >
+                <div
+                    v-for="i in numSlides"
+                    :key="i"
+                    :class="[...slideClass, $style.slide]"
+                >
+                    Slide {{ i }}
+                </div>
+            </VCarousel>
+            <VCarouselControls
+                v-model:index="slideIndex"
+                :length="snapGridLength"
+                :class="$style.controls"
+            />
+        </div>
+    </NuxtStory>
+</template>
+
+<style lang="scss" module>
+.slide {
+    --v-carousel-slide-width: #{rem(400)};
+    --v-carousel-slide-margin-right: #{rem(20)};
+
+    height: rem(200);
+    background-color: lightgray;
+}
+</style>
