24ac3280fec35b7eed8ee3a9b01e4fc92141d96e
Manuel Odelain <manuel@rezo-zero.com>
2024-06-13 11:10:04 +0200

    feat: add VAudio

---
+++ components/molecules/VAudio/Audio.stories.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import document from 'assets/stories/fixtures/documents/audio-01.json'
+</script>
+
+<template>
+    <NuxtStory>
+        <VAudio :document="document" />
+    </NuxtStory>
+</template>
---
+++ components/molecules/VAudio/Spotify.stories.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import document from 'assets/stories/fixtures/documents/spotify-01.json'
+</script>
+
+<template>
+    <NuxtStory>
+        <VAudio :document="document" />
+    </NuxtStory>
+</template>
