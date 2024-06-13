b806ff0280773e6cda9914554740f422a22fe492
Manuel Odelain <manuel@rezo-zero.com>
2024-06-13 12:41:27 +0200

    feat: add VTag

---
+++ components/atoms/VTag/Default.stories.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+    <NuxtStory :class="$style.root">
+        <VTag label="Tag label default" />
+        <VTag label="Tag label sm" size="sm" />
+    </NuxtStory>
+</template>
+
+<style lang="scss" module>
+.root {
+    :global(.nuxt-story__main) {
+        display: flex;
+        align-items: flex-start;
+        gap: rem(20);
+    }
+}
+</style>
---
+++ components/atoms/VTag/VTag.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+
+defineProps({
+    tag: String,
+    label: { type: String },
+    size: { type: String as PropType<'sm' | 'md'>, default: 'md' },
+})
+</script>
+
+<template>
+    <component
+        :is="tag || 'div'"
+        :class="[$style.root, $style[`root--size-${size}`]]"
+    >
+        <slot>{{ label }}</slot>
+    </component>
+</template>
+
+<style lang="scss" module>
+.root {
+    z-index: 1;
+    display: var(--v-tag-display, inline-flex);
+    align-items: center;
+    padding: rem(4 + 4) rem(8 + 4);
+    background-color: var(--v-tag-background-color, #f0f0f0);
+
+    &--size-md {
+        padding: rem(6 + 4) rem(8 + 4);
+    }
+}
+</style>
