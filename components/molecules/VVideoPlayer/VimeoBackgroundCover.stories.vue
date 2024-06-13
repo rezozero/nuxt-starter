25cc4624aa0acf1da46173a82d68328d512498ea
Manuel Odelain <manuel@rezo-zero.com>
2024-06-13 11:05:56 +0200

    feat: add VRoadizVideo + VVideoPlayer

---
+++ components/molecules/VVideoPlayer/VimeoBackgroundCover.stories.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import video from '~/assets/stories/fixtures/documents/vimeo-01.json'
+</script>
+
+<template>
+    <NuxtStory :class="$style.root">
+        <VVideoPlayer
+            :embed-id="video.embedId"
+            :embed-platform="video.embedPlatform"
+            fit="cover"
+            background
+            :class="$style.video"
+        />
+    </NuxtStory>
+</template>
+
+<style lang="scss" module>
+.root {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.video {
+    position: absolute;
+    inset: 0;
+}
+</style>
