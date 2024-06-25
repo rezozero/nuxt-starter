c1599576b77ecbc1fc1ebcab52796f603fd32509
Manuel Odelain <manuel@rezo-zero.com>
2024-06-25 17:37:44 +0200

    feat(picture): do not inherits attrs and pass down the attrs to img

--- components/molecules/VPicture/VPicture.vue
+++ components/molecules/VPicture/VPicture.vue
@@ -2,8 +2,8 @@
 import type { ExtractPropTypes } from 'vue'
 import pick from 'lodash/pick'
 import { pictureProps } from '#image/components/nuxt-picture'
-import VImg from '~/components/molecules/VImg/VImg.vue'
 import { imgProps } from '#image/components/nuxt-img'
+import VImg from '~/components/molecules/VImg/VImg.vue'
 import VPictureSource from '~/components/molecules/VPicture/VPictureSource.vue'
 
 export const vPictureProps = {
@@ -14,6 +14,7 @@ export const vPictureProps = {
 export type VPictureProps = ExtractPropTypes<typeof vPictureProps>
 
 export default defineComponent({
+    inheritAttrs: false,
     props: {
         ...vPictureProps,
     },
@@ -21,30 +22,24 @@ export default defineComponent({
         // Provide props to children (<sources>)
         provide('pictureProps', props)
 
+        const $style = useCssModule()
         const imgFilteredProps = computed(() => pick(props, Object.keys(imgProps)))
         const img = h(VImg, {
             ...imgFilteredProps.value,
             ...props.imgAttrs,
+            ...context.attrs,
         })
         const sources = computed(() => {
             return context.slots.default?.() || h(VPictureSource)
         })
 
-        return () => h('picture', null, [sources.value, img])
+        return () => h('picture', { class: $style.root }, [sources.value, img])
     },
 })
 </script>
 
 <style lang="scss" module>
-.root img {
-    // for switching easily between VImg and VPicture, we use the same css var name
-    max-width: var(--v-img-max-width, 100%); // responsive image
-    height: var(--v-img-height, auto); // responsive image
-    background: var(--v-img-background, var(--v-img-placeholder));
-}
-
-.root--loaded img {
-    // Remove background when image is loaded. This is useful for hiding antialiasing artifacts.
-    --v-img-background: none;
+.root {
+    display: contents;
 }
 </style>
