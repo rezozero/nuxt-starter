6bcb83ba109fa94d5a17acb7f5a5bf3383cdf98a
Manuel Odelain <manuel@rezo-zero.com>
2024-07-17 17:29:42 +0200

    test(useDraggableScroll): add story without scrollbar

---
+++ stories/composables/use-draggable-scroll/NoScrollbar.stories.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const list = ref<HTMLElement | null>(null)
+
+useDraggableScroll(list)
+</script>
+
+<template>
+    <NuxtStory>
+        <ul ref="list" :class="$style.list" class="scrollbar-x-hidden">
+            <li v-for="i in 10" :key="i" :class="$style.item"></li>
+        </ul>
+    </NuxtStory>
+</template>
+
+<style module lang="scss">
+.list {
+    display: flex;
+    width: 600px;
+    overflow-x: auto;
+    padding: 0;
+    gap: 10px;
+    justify-content: flex-start;
+    border: 1px solid black;
+}
+
+.item {
+    width: 300px;
+    flex-shrink: 0;
+    height: 200px;
+    background-color: #ccc;
+}
+</style>
