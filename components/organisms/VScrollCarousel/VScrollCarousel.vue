d1dcc02afa5c06e908890362b19c1e2468837546
Manuel Odelain <manuel@rezo-zero.com>
2024-07-18 19:40:05 +0200

    feat: navigation WIP

--- components/organisms/VScrollCarousel/VScrollCarousel.vue
+++ components/organisms/VScrollCarousel/VScrollCarousel.vue
@@ -1,134 +1,90 @@
 <script setup lang="ts">
-import { getHtmlElement } from '~/utils/ref/get-html-element'
 import { throttle } from 'throttle-debounce'
-
-type ScrollToOptions = {
-    scrollEndCallback?: () => void
-}
 
 export type Slide = {
     left: number
-    snapLeft: number
-    snapRight: number
     right: number
-    el: HTMLElement
+    element: HTMLElement
 }
 
-// DOM ref
+defineProps<{
+    direction?: 'x' | 'y' | 'both'
+    cursor?: boolean
+}>()
+
 const root = ref<HTMLElement | null>(null)
-const firstSlide = ref<HTMLElement | null>(null)
+// const scrollDirection = ref(1)
+let animationFrameId = -1
 
-const unwatchRootEl = watch(root, (el) => {
-    const slideEl = (el?.children[0] as HTMLElement) || null
-    if (!slideEl) return
+// Active slide index
+const activeIndex = defineModel<number>({ default: 0 })
 
-    firstSlide.value = slideEl
-    unwatchRootEl()
+watch(activeIndex, (newValue) => {
+    if (isDragging.value || isTransitioning.value) return
+
+    gotoSlide(newValue)
 })
 
-// Slider data
-const SNAP_OFFSET = 50
-
-const syncedIndex = defineModel<number>({ default: 0 }) // Update this value only when user stop interacting with slider
+// Slides
 const slides = ref<Slide[]>([])
-const scrollDirection = ref(1)
-const isScrolling = ref(false)
-const scrollLeft = ref(0)
 
-const setScrollLeft = () => (scrollLeft.value = getHtmlElement(root)?.scrollLeft || 0)
-
-watch(scrollLeft, (value, oldValue) => {
-    if (value === oldValue) return
-
-    scrollDirection.value = oldValue > value ? -1 : 1
-})
-
-function setSlidesInfos() {
-    const element = getHtmlElement(root)
-
-    if (!element) {
+function setSlides() {
+    if (!root.value) {
         slides.value = []
         return
     }
 
-    const slideList = [...element.children] as HTMLElement[]
-    const firstSlideOffset = slideList[0].getBoundingClientRect().left - element.getBoundingClientRect().left
+    const slideElements = [...root.value.children] as HTMLElement[]
+    const rootRect = root.value.getBoundingClientRect()
 
-    slides.value = slideList.map((child) => {
-        const relativeLeft = child.getBoundingClientRect().left - element.getBoundingClientRect().left
-        const start = relativeLeft - firstSlideOffset + element.scrollLeft
-        const width = child.offsetWidth
+    slides.value = slideElements.map((element) => {
+        const rect = element.getBoundingClientRect()
+        const left = rect.left - rootRect.left
 
         return {
-            left: start,
-            snapLeft: start + SNAP_OFFSET,
-            right: start + width,
-            snapRight: start + width - SNAP_OFFSET,
-            el: child,
+            left: left,
+            right: left + rect.width,
+            element,
         }
     })
 }
 
-const currentIndex = computed(() => {
-    return slides.value.findIndex((slide) => {
-        return scrollLeft.value < slide[scrollDirection.value === 1 ? 'snapLeft' : 'snapRight']
-    })
-})
+// const visibleSlideLength = computed(() => {
+//     return slides.value.filter((slide) => {
+//         return (
+//             slide.left >= Math.ceil(scrollLeft.value) &&
+//             slide.left < scrollLeft.value + (getHtmlElement(root)?.clientWidth || 0)
+//         )
+//     }).length
+// })
+//
+// const lastReachableIndex = computed(() => slides.value.length - visibleSlideLength.value)
 
-const visibleSlideLength = computed(() => {
-    return slides.value.filter((slide) => {
-        return (
-            slide.left >= Math.ceil(scrollLeft.value) &&
-            slide.left < scrollLeft.value + (getHtmlElement(root)?.clientWidth || 0)
-        )
-    }).length
-})
+useMutationObserver(
+    root,
+    (mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList') update()
+        }
+    },
+    { childList: true, attributes: false },
+)
 
-const lastReachableIndex = computed(() => slides.value.length - visibleSlideLength.value)
+useResizeObserver(root, update)
 
-// Slider utils
-const { setStyle, hasScroll, isDragging } = useDraggableScroll(root)
+// Drag
+const { setStyle, isDragging } = useDraggableScroll(root)
 
-const hasTransition = ref(false)
-function scrollTo(index: number, options?: ScrollToOptions) {
-    const element = getHtmlElement(root)
-    const invalidIndex = syncedIndex.value < 0 || syncedIndex.value > lastReachableIndex.value
-    const left = slides.value[index]?.left
-
-    if (!element || invalidIndex || typeof left !== 'number' || hasTransition.value) {
-        return
-    }
-
-    element.addEventListener(
-        'scrollend',
-        () => {
-            options?.scrollEndCallback?.()
-
-            hasTransition.value = false
-            isScrolling.value = false
-
-            // Update v-model only when scroll animation is ended
-            syncedIndex.value = index
-        },
-        { once: true },
-    )
-
-    hasTransition.value = true
-
-    element.scroll({
-        left,
-        behavior: 'smooth',
-    })
-}
-
-// When slider is dragged
 let scrollSnapTypeStored = ''
+
 onMounted(() => {
-    scrollSnapTypeStored = getHtmlElement(root)?.style['scrollSnapType'] || 'x mandatory'
+    scrollSnapTypeStored = root.value?.style['scrollSnapType'] || ''
 })
 
 watch(isDragging, () => {
     if (!root.value) return
+
+    cancelTransition()
 
     if (isDragging.value) {
         // Disable scroll snap when dragging (scroll snap prevent manual scroll)
@@ -139,7 +95,8 @@ watch(isDragging, () => {
         // Restore scroll snap
         root.value.style.scrollSnapType = scrollSnapTypeStored
 
-        requestAnimationFrame(() => {
+        // Chrome and Safari need help to smooth scroll to the right position (they jump directly to the end)
+        animationFrameId = requestAnimationFrame(() => {
             if (!root.value) return
 
             // Safari need to force reflow to compute next scroll position
@@ -150,7 +107,6 @@ watch(isDragging, () => {
             const destX = root.value.scrollLeft
 
             // Firefox has already the right scroll position
-            // But Chrome and Safari need help to smooth scroll to the right position
             if (destX === currentX) return
 
             // Disable scroll snap when dragging (scroll snap prevent manual scroll)
@@ -158,84 +114,81 @@ watch(isDragging, () => {
 
             root.value.scrollTo({ left: currentX })
 
-            requestAnimationFrame(() => {
+            animationFrameId = requestAnimationFrame(() => {
                 if (!root.value) return
 
-                root.value.addEventListener(
-                    'scrollend',
-                    () => {
-                        if (root.value) root.value.style.scrollSnapType = scrollSnapTypeStored
-                    },
-                    { once: true },
-                )
-
                 // Go to the right scroll position with smooth transition
-                root.value!.scrollTo({ left: destX, behavior: 'smooth' })
+                scrollTo({ left: destX })
             })
         })
     }
 })
 
-// function onMouseUp() {
-//     isDragging.value = false
-//     scrollTo(currentIndex.value, { scrollEndCallback: onDragTransitionEnd })
-// }
+// Transition
+const isTransitioning = ref(false)
 
-// Update slide index and slide direction
-const onScrollCallback = throttle(100, onScroll)
+function cancelTransition() {
+    cancelAnimationFrame(animationFrameId)
 
-function onScroll() {
-    console.log(isDragging.value)
-    // onScrollCallback()
+    isTransitioning.value = false
+}
+
+function scrollTo(options: ScrollToOptions) {
+    if (!root.value) return
+
+    cancelTransition()
+
+    root.value.style.scrollSnapType = 'none'
+
+    isTransitioning.value = true
+
+    // Go to the right scroll position with smooth transition
+    root.value.scrollTo({ ...options, behavior: options.behavior ?? 'smooth' })
 }
 
 function onScrollEnd() {
-    if (isDragging.value || hasTransition.value) return
+    if (isDragging.value) return
 
-    isScrolling.value = false
+    isTransitioning.value = false
 
-    getHtmlElement(root)!.style['scrollSnapType'] = scrollSnapTypeStored
-
-    // Update VModel on native scroll
-    syncedIndex.value = currentIndex.value
+    if (root.value) root.value.style.scrollSnapType = scrollSnapTypeStored
 }
 
-watch(syncedIndex, (value) => {
-    if (value === currentIndex.value || isScrolling.value || isDragging.value) return
+// Navigation
+function gotoSlide(slideIndex: number) {
+    const left = slides.value[slideIndex]?.left
 
-    scrollTo(value)
+    if (typeof left !== 'number') return
+
+    scrollTo({ left })
+}
+
+function gotoNextSlide() {}
+
+function gotoPreviousSlide() {}
+
+// Update slide index and slide direction
+const onScroll = throttle(100, computeActiveSlideIndex)
+
+function computeActiveSlideIndex() {}
+
+onMounted(() => {
+    update()
+    computeActiveSlideIndex()
+    gotoSlide(activeIndex.value)
 })
 
-// Wrapper and slide listener
-function updateSlides() {
-    setSlidesInfos()
+function update() {
+    setSlides()
     setStyle()
 }
 
-useMutationObserver(
-    root,
-    (mutations) => {
-        for (const mutation of mutations) {
-            if (mutation.type === 'childList') updateSlides()
-        }
-    },
-    { childList: true, attributes: false },
-)
-
-useResizeObserver(firstSlide, updateSlides)
-watch(hasScroll, () => {
-    updateSlides()
-
-    if (syncedIndex.value === currentIndex.value) return
-    scrollTo(syncedIndex.value)
-})
-
 // Expose
-defineExpose<{ slides: Ref<Slide[]> }>({ slides })
+defineExpose({ gotoNextSlide, gotoPreviousSlide, gotoSlide, update })
 </script>
 
 <template>
-    <div ref="root" :class="$style.root">
+    <div ref="root" :class="$style.root" @scroll="onScroll" @scrollend="onScrollEnd">
         <slot :slide-class="$style.slide" />
     </div>
 </template>
