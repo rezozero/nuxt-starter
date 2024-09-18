9b9bb61ba95ae08d96183fcbf461cc5711e883a2
julientison <julientison@rezo-zero.com>
2024-09-18 12:42:14 +0200

    chore: eslint error

--- components/organisms/VCarousel/VCarousel.vue
+++ components/organisms/VCarousel/VCarousel.vue
@@ -91,7 +91,7 @@ async function createSwiper() {
                 if (swiper.isEnd) emit('onSlideEnd')
             },
             init: (swiper: Swiper): void => {
-                swiper && options?.on?.init?.(swiper)
+                if (swiper) options?.on?.init?.(swiper)
                 resizeObserver?.observe(swiper.wrapperEl)
                 checkSwiperActivation(swiper)
                 updateSlidesFocusCapability(swiper)
