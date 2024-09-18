169eda03e30a6e593f6e50ce3eb3bd31b1e4aa01
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2024-09-18 18:06:52 +0200

    test(useMockRequest): add story

---
+++ stories/composables/use-mock-request.stories.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { http, HttpResponse } from 'msw'
+
+useMockRequest(
+    http.get('*/api/fake-request', () => {
+        return HttpResponse.json({
+            title: 'My response',
+        })
+    }),
+)
+
+$fetch('api/fake-request')
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+</script>
+
+<template>
+    <NuxtStory />
+</template>
+
+<style module lang="scss">
+
+</style>
