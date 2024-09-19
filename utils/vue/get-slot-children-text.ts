5f7128793a1b5c83eb28320f1961b115667aee0e
Julien Tison <67205807+Illawind@users.noreply.github.com>
2024-09-19 13:05:28 +0200

    fix(VMarkdown): update marked v13 breaking changes (#51)

--- utils/vue/get-slot-children-text.ts
+++ utils/vue/get-slot-children-text.ts
@@ -1,12 +1,44 @@
-import type { VNodeNormalizedChildren } from 'vue'
+import type { Slots, VNode, VNodeNormalizedChildren } from 'vue'
+import type { VNodeChild } from '@vue/runtime-core'
 
-// TODO: fix type
-export function getSlotChildrenText(children: VNodeNormalizedChildren) {
-    return children
-        ?.map((node) => {
-            if (!node.children || typeof node.children === 'string') return node.children || ''
-            else if (Array.isArray(node.children)) return getSlotChildrenText(node.children)
-            else if (node.children.default) return getSlotChildrenText(node.children.default())
-        })
-        .join('')
+function getChildNodeText(childNode: VNodeNormalizedChildren): string | string[] | undefined {
+    if (!childNode) return
+    else if (typeof childNode === 'string') return childNode
+    else if (Array.isArray(childNode)) return childNode.filter(n => !!n).map(vNodeArrayChildren => getNode(vNodeArrayChildren)).join(' ')
+}
+
+function getNode(node: VNodeChild): string | string[] | undefined {
+    if (!node) return
+    else if (typeof node === 'string') return node
+    else if (typeof node === 'object' && 'children' in node && node.children?.length) return getChildNodeText(node.children)
+    return
+}
+
+function getNodeInnerText(node: VNode | Slots) {
+    const innerSlotsChildren = isSlots(node.children) && getNamedSlots(node.children)
+
+    if (!node) return
+    else if (innerSlotsChildren) return getChildNodeText(innerSlotsChildren)
+    else if (node.children?.length) return getChildNodeText(node.children as VNodeNormalizedChildren)
+}
+
+function isSlots(item: unknown): item is Slots {
+    return !!(!!item && typeof item === 'object' && ((item as { default: () => void })?.['default']?.()))
+}
+
+function getNamedSlots(slots: Slots, name = 'default') {
+    const slot = slots[name]?.()
+
+    if (!slot || !slot.length) return
+
+    return slot
+}
+
+export function getSlotsInnerText(slots: Slots, name = 'default') {
+    const slot = getNamedSlots(slots, name)
+    if (!slot) return ''
+
+    return slot.map((slot) => {
+        return getNodeInnerText(slot)
+    }).join(' ')
 }
