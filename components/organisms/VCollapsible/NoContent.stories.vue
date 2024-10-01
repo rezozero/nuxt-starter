e1a2c94f25a4bf65cf9978594ec0a4afb6caec37
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2024-10-01 12:14:26 +0200

    feat: add VCollapsible

---
+++ components/organisms/VCollapsible/NoContent.stories.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtStory layout="fullscreen">
+        <VCollapsible
+            label="This is the label"
+            collapsed
+            style="max-width: 600px"
+        />
+    </NuxtStory>
+</template>
