7ef0f792a243542b2e89ade9d8948e7d591caaf2
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2024-10-03 11:49:41 +0200

    feat: add loading animation

---
+++ stories/styles/LoadingAnimation.stories.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <NuxtStory>
+        <NuxtStoryVariant title="Global class">
+            <div
+                class="loading-animation"
+                :class="$style.block"
+            />
+        </NuxtStoryVariant>
+        <NuxtStoryVariant title="Global mixin">
+            <div
+                :class="[$style.block, $style['block--loading-animation']]"
+            />
+        </NuxtStoryVariant>
+        <NuxtStoryVariant title="Text placeholder">
+            <div
+                :class="[$style.text]"
+            />
+        </NuxtStoryVariant>
+    </NuxtStory>
+</template>
+
+<style module lang="scss">
+.block {
+    width: 100%;
+    max-width: 500px;
+    aspect-ratio: 300/200;
+
+    &--loading-animation {
+        @include loading-animation;
+    }
+}
+
+.text {
+    @include loading-animation-text-placeholder();
+
+    width: 15ch;
+}
+</style>
