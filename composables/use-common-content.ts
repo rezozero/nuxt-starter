a5e09dd4e1b3397d0d7a1b7f54c6c6e025ad5254
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2024-11-19 16:16:50 +0100

    fix(useCommonContent): use cached data

--- composables/use-common-content.ts
+++ composables/use-common-content.ts
@@ -4,10 +4,14 @@ import type { ValueOf } from '~/utils/types'
 export const COMMON_CONTENT_KEY = 'commonContent'
 
 export async function useCommonContent() {
+    const nuxtApp = useNuxtApp()
     const { data } = await useAsyncData<CommonContent>(COMMON_CONTENT_KEY, () => {
         const fetch = useRoadizFetchFactory()
 
         return fetch('/common_content')
+    }, {
+        getCachedData: key => nuxtApp.static.data[key] ?? nuxtApp.payload.data[key], // no re-fetch data if the key is already in the payload
+        dedupe: 'defer', // wait for the first request to finish before making another request
     })
     const homeItem = computed(() => data.value?.home)
     const head = computed(() => data.value?.head)
