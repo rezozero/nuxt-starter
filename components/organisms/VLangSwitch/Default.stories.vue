62a96bb50754255aa07321fdc743d4d182f1a0e4
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-01-13 17:14:52 +0100

    feat: add lang switch

---
+++ components/organisms/VLangSwitch/Default.stories.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+useAlternateLinks([
+    { locale: 'en', url: '/en' },
+    { locale: 'fr', url: '/fr' },
+])
+</script>
+
+<template>
+    <NuxtStory>
+        <VLangSwitch />
+    </NuxtStory>
+</template>
