74913a0fb85cb30a545b81f18c258dc9ab96fed5
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-01-14 15:05:59 +0100

    feat: add lang switch (#241)
    * feat: add lang switch
    
    * chore: remove unused classes

---
+++ components/organisms/VLangSwitch/VLangSwitch.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+const { locale } = useI18n()
+const { availableAlternateLinks } = useAlternateLinks()
+</script>
+
+<template>
+    <div
+        v-if="availableAlternateLinks.length > 1"
+    >
+        <nav>
+            <ul>
+                <li
+                    v-for="alternateLink in availableAlternateLinks"
+                    :key="alternateLink.url"
+                    :class="[$style.item, alternateLink.locale === locale && $style['item--current']]"
+                >
+                    <a
+                        :href="alternateLink.url"
+                        :hreflang="alternateLink.locale"
+                    >
+                        {{ alternateLink.locale }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss" module>
+.item {
+    &--current {
+        opacity: .5;
+    }
+
+    &::first-letter {
+        text-transform: uppercase;
+    }
+}
+</style>
