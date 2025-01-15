0a5a7df1465c6e8a81be6a50fc5fb87172cf0c2c
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-01-15 14:15:54 +0100

    feat(VRoadizLink): more generic types

--- types/app.d.ts
+++
@@ -1,8 +0,0 @@
-import type { RoadizNodesSources } from '@roadiz/types'
-import type { UnionToIntersection } from '~/utils/types'
-
-export type MenuNodeType = RoadizNodesSources
-export type MenuNodeKeyMerged = Partial<UnionToIntersection<MenuNodeType>>
-
-export type ReachableItem = RoadizNodesSources
-export type ReachableItemMerged = Partial<UnionToIntersection<ReachableItem>>
