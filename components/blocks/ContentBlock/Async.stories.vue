74abae4330e18547e96c99a6859506c0324bcd56
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-01-16 08:50:11 +0100

    test: add story

---
+++ components/blocks/ContentBlock/Async.stories.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { RoadizWalker } from '@roadiz/types'
+
+const walker: RoadizWalker = {
+    // @ts-expect-error - This is a mock object
+    item: {
+        title: 'Title',
+    },
+    children: [],
+}
+</script>
+
+<template>
+    <NuxtStory :class="$style.root">
+        <ContentBlock
+            :walker="walker"
+            :index="0"
+            :class="$style.block"
+        />
+    </NuxtStory>
+</template>
+
+<style module lang="scss">
+.root {
+    padding-top: 200vh;
+}
+
+.block {
+    border: 1px solid red;
+}
+</style>
