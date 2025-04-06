76c521ba603be6cb44550be14d6db0dd38c1f7cf
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-04-06 17:09:27 +0200

    feat: improve page data (#286)
    * fix: alternate links typing
    
    * feat: improve page data
    
    * fix(cache control): valid age detection

--- components/organisms/VDefaultPage/VDefaultPage.vue
+++ components/organisms/VDefaultPage/VDefaultPage.vue
@@ -1,15 +1,21 @@
 <script lang="ts" setup>
-import type { RoadizNodesSources, RoadizWalker } from '@roadiz/types'
+import type { PageEntityProps } from '~/types/app'
+import type { NSPage } from '~/types/roadiz'
+import { getBlockCollection } from '~/utils/roadiz/block'
 
-defineProps<{
-    blocks: RoadizWalker[]
-    entity: RoadizNodesSources
-}>()
+const { webResponse } = defineProps<PageEntityProps>()
+
+const page = computed(() => webResponse?.item as NSPage)
+const blocks = computed(() => (webResponse?.blocks && getBlockCollection(webResponse.blocks)) || [])
 </script>
 
 <template>
     <div>
-        {{ entity.title || 'VDefaultPage' }}
+        <h1>{{ page?.title || 'VDefaultPage' }}</h1>
+        <VRoadizBlockFactory
+            v-if="blocks.length"
+            :blocks="blocks"
+        />
     </div>
 </template>
 
--- composables/use-current-page.ts
+++ composables/use-current-page.ts
@@ -1,9 +1,5 @@
 import type { Page } from '~/composables/use-page'
 
-export function useCurrentPage(page?: Page) {
-    const currentPage = useState<Page>('currentPage', () => ({}))
-
-    if (page) currentPage.value = page
-
-    return currentPage
+export function useCurrentPage() {
+    return useState<Page>('currentPage', () => shallowRef({}))
 }
