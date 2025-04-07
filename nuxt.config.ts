003481566f9eed37b5d80c8ea2d37264b33697b9
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-04-07 15:58:54 +0200

    chore: eslint fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,8 @@ export default defineNuxtConfig({
             path: '~/components/blocks',
             // As the blocks are used in the CMS, we need to import them globally.
             global: true,
-            // To avoid importing unnecessary components that are present in the blocks/ folder (e.g. the story files), we add a `global` suffix to the block components.
+            // To avoid importing unnecessary components that are present in the blocks/ folder (e.g. the story files),
+            // we add a `global` suffix to the block components.
             // Therefore only these components will be included into the production bundles.
             extensions: ['.global.vue'],
         },
