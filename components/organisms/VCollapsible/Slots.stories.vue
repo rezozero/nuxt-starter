92f60fcb595a09b752f953917783584f9263b775
timothe joubert <timothejoubert26@gmail.com>
2025-06-17 15:12:05 +0200

    chore: run lint-fix

--- components/organisms/VCollapsible/Slots.stories.vue
+++ components/organisms/VCollapsible/Slots.stories.vue
@@ -1,32 +1,38 @@
 <template>
     <NuxtStory layout="fullscreen">
-        <VCollapsible style="max-width: 600px">
-            <template #control="{ isCollapsed, ariaControl, disabled, toggle }">
-                <button
-                    :class="$style.button"
-                    :aria-expanded="!isCollapsed"
-                    :aria-controls="ariaControl"
-                    :disabled="disabled"
-                    @click="toggle"
-                >
-                    Toggle
-                </button>
-            </template>
-            <div>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, delectus earum in minus soluta
-                ullam! Atque consectetur fuga totam ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Cupiditate, delectus earum in minus soluta
-                ullam! Atque consectetur fuga totam ullam.
-            </div>
-        </VCollapsible>
+        <div :class="$style.wrapper">
+            <VCollapsible>
+                <template #button="{ isCollapsed, ariaControls, disabled, toggle }">
+                    <button
+                        :class="$style.button"
+                        :aria-expanded="!isCollapsed"
+                        :aria-controls="ariaControls"
+                        :disabled="disabled"
+                        @click="toggle"
+                    >
+                        Toggle
+                    </button>
+                </template>
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, delectus earum in minus soluta
+                    ullam! Atque consectetur fuga totam ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Cupiditate, delectus earum in minus soluta
+                    ullam! Atque consectetur fuga totam ullam.
+                </div>
+            </VCollapsible>
+        </div>
     </NuxtStory>
 </template>
 
 <style lang="scss" module>
+.wrapper {
+    max-width: 600px;
+}
+
 .button {
-    margin-block: 20px;
     width: 100%;
-    font-size: 24px;
     cursor: pointer;
+    font-size: 24px;
+    margin-block: 20px;
 }
 </style>
