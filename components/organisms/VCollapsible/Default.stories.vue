ab5d7d31582d90f46c47faf41ecb6e57c785f6c0
Timothe Joubert <35274260+timothejoubert@users.noreply.github.com>
2025-06-18 11:56:02 +0200

    refactor: improve VCollapsible a11y (#370)
    * refactor: improve VCollapsible a11y
    
    * refactor: fix typo and remove root element
    
    * chore: run lint-fix
    
    * chore: adapt stories
    
    * chore: better className and remove unused prop
    
    ---------
    
    Co-authored-by: timothe joubert <timothejoubert26@gmail.com>

--- components/organisms/VCollapsible/Default.stories.vue
+++ components/organisms/VCollapsible/Default.stories.vue
@@ -1,13 +1,14 @@
 <template>
     <NuxtStory layout="fullscreen">
-        <VCollapsible
-            label="This is the label"
-            style="max-width: 600px"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, delectus earum in minus soluta
-            ullam! Atque consectetur fuga totam ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Cupiditate, delectus earum in minus soluta
-            ullam! Atque consectetur fuga totam ullam.
-        </VCollapsible>
+        <div style="max-width: 600px">
+            <VCollapsible
+                label="This is the label"
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, delectus earum in minus soluta
+                ullam! Atque consectetur fuga totam ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Cupiditate, delectus earum in minus soluta
+                ullam! Atque consectetur fuga totam ullam.
+            </VCollapsible>
+        </div>
     </NuxtStory>
 </template>
--- components/organisms/VCollapsible/NoContent.stories.vue
+++ components/organisms/VCollapsible/NoContent.stories.vue
@@ -1,9 +1,10 @@
 <template>
     <NuxtStory layout="fullscreen">
-        <VCollapsible
-            label="This is the label"
-            collapsed
-            style="max-width: 600px"
-        />
+        <div style="max-width: 600px">
+            <VCollapsible
+                label="This is the label"
+                collapsed
+            />
+        </div>
     </NuxtStory>
 </template>
