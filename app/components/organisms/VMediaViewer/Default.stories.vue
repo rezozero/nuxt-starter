2d622da1e2111c85e64eeb44af75e9def3b95270
timothe joubert <timothejoubert26@gmail.com>
2025-09-03 17:49:43 +0200

    chore: WIP simplify style and add a11y attributes

--- app/components/organisms/VMediaViewer/Default.stories.vue
+++ app/components/organisms/VMediaViewer/Default.stories.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import type { RoadizDocument } from '@roadiz/types'
-import { useMediaViewer } from '~/composables/use-media-viewer'
 import image from '~/assets/stories/fixtures/documents/image-01.json'
 import imagePortrait from '~/assets/stories/fixtures/documents/image-portrait.json'
 import video from '~/assets/stories/fixtures/documents/video-01.json'
+import { useMediaViewer } from '~/composables/use-media-viewer'
 
-const documents = [image, imagePortrait, video, image, video, image] as RoadizDocument[]
+const imageWithLongDesc = {
+    ...image,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore possimus quaerat non nesciunt perspiciatis. Cumque officia sequi consequatur corrupti nihil id quisquam similique asperiores quia dolorem aut voluptatibus ducimus aspernatur, voluptas mollitia expedita. Ullam tenetur labore assumenda at? Porro, neque? Temporibus, voluptates incidunt quam, dolorum quos a consequatur id, enim veniam perferendis impedit suscipit. Nulla dignissimos corrupti similique voluptate quae molestiae iure itaque, velit maxime officiis ut a nobis iusto est! Veniam quas, dolorem quod harum dolore id, blanditiis officia accusamus dolor corrupti, quisquam optio eos ex maiores totam? Voluptatum quaerat sit hic rem quo molestiae eaque, modi asperiores velit.',
+}
+
+const documents = [image, imageWithLongDesc, imagePortrait, video, image, video, image] as RoadizDocument[]
 const { isOpen, open } = useMediaViewer()
 
 function onOpenClick() {
--- app/components/organisms/VMediaViewer/VMediaViewer.vue
+++ app/components/organisms/VMediaViewer/VMediaViewer.vue
@@ -4,33 +4,33 @@ import type Plyr from 'plyr'
 import { isVideo } from '~/utils/roadiz/document'
 
 const id = useId()
+const carouselId = useId()
 
 const theme = 'dark'
-
 const { themeClass } = useThemeProvider({ preferredTheme: theme })
 
+// Carousel controls
 const { documents, slideIndex, close, nextSlide, previousSlide } = useMediaViewer()
-
 const snapLength = ref(documents.value?.length || 0)
+const { prevButtonAttrs, nextButtonAttrs } = useCarouselControls({
+    snapLength,
+    index: slideIndex,
+})
 
 useEventListener('keyup', onKeyPressed)
-
 function onKeyPressed(event: KeyboardEvent) {
     if (event.key === 'Escape') close()
     else if (event.key === 'ArrowLeft') previousSlide()
     else if (event.key === 'ArrowRight') nextSlide()
 }
 
+// Data
 const copyrightContent = computed(() => {
     return documents.value?.[slideIndex.value]?.copyright
 })
 
-const description = computed(() => {
-    return documents.value?.[slideIndex.value]?.description
-})
-
+// Video play state
 const plyrInstances = ref<{ index: number, plyr: Plyr }[]>([])
-
 function onPlyrReady(plyr: Plyr, index: number) {
     const isAlreadyInit = plyrInstances.value.find(item => item.index === index)
 
@@ -41,7 +41,6 @@ function onPlyrReady(plyr: Plyr, index: number) {
         })
     }
 }
-
 watch(
     slideIndex,
     (currentSlideIndex) => {
@@ -54,12 +53,11 @@ watch(
     { flush: 'post' },
 )
 
+// Modal state
 const root = ref<HTMLDialogElement | null>(null)
-
 onMounted(() => {
     root.value?.showModal()
 })
-
 onBeforeUnmount(() => {
     plyrInstances.value = []
     root.value?.close()
@@ -71,22 +69,31 @@ onBeforeUnmount(() => {
         :id="id"
         ref="root"
         :class="[$style.root, description && $style['root--with-description'], themeClass]"
+        :aria-label="$t('media_viewer.aria_label')"
     >
         <VButton
             :class="$style.close"
             icon-name="cross-small"
             emphasis="low"
+            :aria-label="$t('v_media_viewer.close_aria_label')"
             @click="close"
         />
-        <VCarouselControls
-            v-if="documents?.length && documents.length > 1"
-            v-model="slideIndex"
-            :snap-length="snapLength"
-            :class="$style.controls"
-            :theme="theme"
-        />
+        <template v-if="snapLength > 1">
+            <VButton
+                v-bind="prevButtonAttrs"
+                :class="[$style.button, $style['button--prev']]"
+                :aria-controls="carouselId"
+            />
+            <VButton
+                v-bind="nextButtonAttrs"
+                :class="[$style.button, $style['button--next']]"
+                :aria-controls="carouselId"
+            />
+        </template>
+
         <VCarousel
             v-if="documents?.length"
+            :id="carouselId"
             v-slot="{ slideClass }"
             v-model:index="slideIndex"
             v-model:snap-length="snapLength"
@@ -115,14 +122,15 @@ onBeforeUnmount(() => {
                     v-if="document.description"
                     :class="$style.description"
                 >
-                    <VClampedText
+                    <LazyVClampedText
                         :content="document.description"
                         :lines="1"
                     />
                 </div>
             </div>
         </VCarousel>
-        <VCopyright
+
+        <LazyVCopyright
             v-if="copyrightContent"
             :class="$style.copyright"
             :content="copyrightContent"
@@ -133,75 +141,81 @@ onBeforeUnmount(() => {
 
 <style lang="scss" module>
 @use "assets/scss/mixins/theme" as *;
-@use "assets/scss/mixins/include-media" as *;
+@use 'sass:math';
 
 .root {
-    --v-img-height: #{calc(100% - #{px-to-rem(262)})};
+    --v-media-viewer-slide-padding-block: 130px;
+    --v-media-viewer-slide-max-height: calc(100% - var(--v-media-viewer-slide-padding-block) * 2);
 
     position: fixed;
     z-index: 101;
     display: flex;
+    overflow: hidden;
+    min-width: 100%;
     max-width: 100%;
-    height: 100%;
-    max-height: 100%;
-    flex-direction: column;
-    padding: var(--gutter);
+    min-height: 100%;
+    max-height: 100vh;
+    padding: initial;
     border: none;
-    background-color: var(--theme-color-surfaces-primary);
-    color: var(--theme-color-content-primary);
+    margin: initial;
+    background-color: var(--colors-surface-primary, #010101);
+    color: var(--colors-content-primary, #FFF);
     inset: 0;
 
     @include theme-variants($match: 'surfaces-' 'content-' 'lines-' 'links-' 'control-');
-
-    &::after {
-        position: absolute;
-        z-index: -1;
-        background-color: var(--theme-color-surfaces-primary);
-        content: '';
-        inset: 0;
-    }
-
-    @include media('>=lg') {
-        padding: 0 calc((100vw - var(--gutter) * 4) / 12 + var(--gutter) * 2);
-    }
 }
 
 .close {
     position: absolute;
     z-index: 2;
-    top: px-to-rem(16);
+    top: 16px;
     right: var(--gutter);
 
     @include media('>=lg') {
-        top: px-to-rem(24);
-        right: px-to-rem(24);
+        top: var(--spacing-md, 24px);
+        right: var(--spacing-md, 24px);
+    }
+}
+
+.button {
+    @include media('<lg') {
+        display: none;
+    }
+
+    @include media('>=lg') {
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        transform: translateY(-50%);
+
+        &--prev {
+            left: var(--spacing-lg, 2rem);
+        }
+
+        &--next {
+            right: var(--spacing-lg, 2rem);
+        }
     }
 }
 
 .carousel {
-    --v-carousel-slide-width: calc(100vw - var(--gutter) * 2);
-    --v-carousel-slide-margin-right: var(--gutter);
+    --v-carousel-slide-width: 100%;
+    --v-carousel-slide-margin-right: 0;
     --v-carousel-swiper-wrapper-height: 100%;
-
-    overflow: hidden;
-    height: 100%;
-    align-items: center;
-
-    @include media('>=lg') {
-        --v-carousel-slide-width: calc((100vw - var(--gutter) * 4) * 10 / 12);
-        --v-carousel-slide-height: 100vh;
-        --v-carousel-slide-margin-right: calc(#{flex-grid(1, 12, 'vw', true)} + var(--gutter) * 3);
-    }
 }
 
 .slide {
+    --v-img-height: 100%;
+
     position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: px-to-rem(24);
-    padding-block: px-to-rem(130);
+    gap: var(--spacing-md, 24px);
+    height: 100%;
+    padding-block: var(--v-media-viewer-slide-padding-block);
+    padding-inline: var(--gutter);
 
     // style iframe for audio
     > iframe {
@@ -210,7 +224,6 @@ onBeforeUnmount(() => {
         max-height: 100%;
     }
 }
-
 .media {
     z-index: 1;
     display: flex;
@@ -231,11 +244,11 @@ onBeforeUnmount(() => {
 .description {
     position: absolute;
     z-index: 1;
-    bottom: px-to-rem(32);
-    left: 0;
-    width: 100%;
+    bottom: var(--spacing-lg, 32px);
+    width: min(100%, 80ch);
     text-align: center;
-
+    background-color: var(--colors-surface-primary, #010101);
+    padding-inline: var(--spacing-lg, 32px);
     > div:first-child * {
         opacity: 0.8;
     }
@@ -245,49 +258,13 @@ onBeforeUnmount(() => {
     }
 }
 
-.controls {
-    position: absolute;
-    inset: 0;
-    transform: initial;
-
-    > div {
-        position: absolute;
-        top: px-to-rem(28);
-        left: 50%;
-        transform: translateX(-50%);
-
-        @include media('>=md') {
-            top: px-to-rem(34);
-        }
-    }
-
-    > button {
-        display: none;
-
-        @include media('>=lg') {
-            position: absolute;
-            top: 50%;
-            display: block;
-            transform: translateY(-50%);
-
-            &:first-of-type {
-                left: var(--gutter);
-            }
-
-            &:last-of-type {
-                right: var(--gutter);
-            }
-        }
-    }
-}
-
 .copyright {
-    --v-copyright-right: #{px-to-rem(20)};
-    --v-copyright-bottom: #{px-to-rem(26)};
+    --v-copyright-right: var(--spacing-md, 24px);
+    --v-copyright-bottom: var(--spacing-md, 24px);
 
     @include media('>=md') {
-        --v-copyright-right: #{px-to-rem(32)};
-        --v-copyright-bottom: #{px-to-rem(32)};
+        --v-copyright-right: var(--spacing-lg, 32px);
+        --v-copyright-bottom: var(--spacing-lg, 32px);
     }
 }
 </style>
