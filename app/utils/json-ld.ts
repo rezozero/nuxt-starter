5b01de32ee22234368f388589b4807eb68f01b53
timothe joubert <timothejoubert26@gmail.com>
2025-09-04 14:33:44 +0200

    refactor: fix type error on removeNullishNestedKeys

--- app/utils/json-ld.ts
+++ app/utils/json-ld.ts
@@ -3,25 +3,46 @@ import { toDisplayString } from 'vue'
 
 type StructuredDataContent = Record<string, unknown>
 
-const isNullishValue = (v: unknown) => {
+const isNullishValue = (v: unknown): boolean => {
     if (typeof v === 'number') return false
-    return v === null || v === undefined || !Object.keys(v).length || (Array.isArray(v) && !v.length)
+    if (v === null || v === undefined) return true
+    if (Array.isArray(v)) return v.length === 0
+    if (typeof v === 'object') return Object.keys(v).length === 0
+    return false
 }
 
-function removeNullishNestedKeys(obj: StructuredDataContent) {
-    Object.keys(obj).forEach((key) => {
-        const value = obj[key]
+export function removeNullishNestedKeys<T extends StructuredDataContent>(obj: T): T {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+        if (isNullishValue(value)) {
+            return acc
+        }
 
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        if (isNullishValue(value)) delete obj[key]
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        else if (Array.isArray(value) && value.every(v => isNullishValue(v))) delete obj[key]
-        else if (value && typeof value === 'object') removeNullishNestedKeys(value as StructuredDataContent)
-    })
+        if (Array.isArray(value)) {
+            const cleanedArr = value
+                .map(v =>
+                    typeof v === 'object' && v !== null
+                        ? removeNullishNestedKeys(v as StructuredDataContent)
+                        : v)
+                .filter(v => !isNullishValue(v))
 
-    return obj
+            if (cleanedArr.length > 0) {
+                (acc as StructuredDataContent)[key] = cleanedArr
+            }
+            return acc
+        }
+
+        if (typeof value === 'object' && value !== null && Object.keys(value).length) {
+            const cleanedObj = removeNullishNestedKeys(value as StructuredDataContent)
+            if (!isNullishValue(cleanedObj)) {
+                (acc as StructuredDataContent)[key] = cleanedObj
+            }
+            return acc
+        }
+
+        (acc as StructuredDataContent)[key] = value
+        return acc
+    }, {} as T)
 }
-
 export function getJsonLdScriptContent(content: MaybeRef<StructuredDataContent | unknown[]>) {
     const value = toValue(content)
 
