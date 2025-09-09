748532e34bdf2de47cdbc14f293fea53c42072df
timothe joubert <timothejoubert26@gmail.com>
2025-09-09 16:34:44 +0200

    chore: add stories

---
+++ app/components/organisms/VForm/FriendlyCaptcha.stories.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { JsonSchemaExtended } from '~~/types/json-schema'
+
+const config = useRuntimeConfig()
+config.public.frcCaptcha.siteKey = 'FCMTFP5GAQI2L2E1'
+
+const schema = {
+    title: 'test_tous_les_types_de_champs',
+    type: 'object',
+    properties: {
+        'text': {
+            type: 'string',
+            title: 'Text',
+        },
+        'frc-captcha-response': {
+            type: 'string',
+            title: 'friendly_captcha',
+            propertyOrder: 15,
+        },
+    },
+    required: ['frc-captcha-response'],
+}
+</script>
+
+<template>
+    <NuxtStory>
+        <VForm :schema="(schema as JsonSchemaExtended)" />
+    </NuxtStory>
+</template>
---
+++ app/components/organisms/VForm/ReCaptcha.stories.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { JsonSchemaExtended } from '~~/types/json-schema'
+
+const config = useRuntimeConfig()
+config.public.gRecaptcha.siteKey = '6LeOgMMrAAAAAIkfOLG7S17XMgRtbZLgpNcfQdTp'
+
+const schema = {
+    title: 'test_tous_les_types_de_champs',
+    type: 'object',
+    properties: {
+        'text': {
+            type: 'string',
+            title: 'Text',
+        },
+        'g-recaptcha-response': {
+            type: 'string',
+            title: 'recaptcha',
+            propertyOrder: 15,
+        },
+    },
+    required: ['g-recaptcha-response'],
+}
+
+function customSubmit(data) {
+    console.log('customSubmit', data)
+}
+</script>
+
+<template>
+    <NuxtStory>
+        <VForm
+            :submit-callback="customSubmit"
+            :schema="(schema as JsonSchemaExtended)"
+        />
+    </NuxtStory>
+</template>
