863d40a5900445fad59eaea98f186846717a4e2d
Timothe Joubert <35274260+timothejoubert@users.noreply.github.com>
2025-09-11 17:01:48 +0200

    feat: handle captcha providers (#449)
    * feat: add captcha utils files and providers config
    
    * feat: add usefull captcha composables
    
    * feat: update components with new captcha files
    
    * chore: add stories
    
    * refactor: use common function to init captcha providers
    
    * chore: clear usage, logs and fix execute function
    
    * build: remove vue-recaptcha-v3 package
    
    * chore: remove specific recaptcha composables
    
    * chore: update siteKey test
    
    * chore: remove empty lines
    
    * chore: remove duplicate define object check
    
    * chore: better error log message
    
    * chore: rename methods name
    
    * build: update pnpm-lock file
    
    * refactor: update CaptchaProvider data, rename variables and change loadscript call timing
    
    * refactor: track only current captcha provider userConsent data
    
    * refactor: rename composable and keep roadiz captcha key in dedicated file
    
    * fix: fix loadScript promise when script has noModule
    
    * refactor: rename provider captcha files, and use dedicated file for captcha key constant
    
    * chore: rename variable
    
    * refactor: simplify defineCaptchaProvider default value setup
    
    * chore: remove log
    
    * chore: clarify log
    
    ---------
    
    Co-authored-by: timothe joubert <timothejoubert26@gmail.com>

---
+++ app/components/molecules/VCaptchaInput/FriendlyCaptcha.stories.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const config = useRuntimeConfig()
+config.public.friendlyCaptcha.siteKey = 'FCMV995O03V7RIMQ'
+</script>
+
+<template>
+    <NuxtStory>
+        <NuxtStoryVariant>
+            <VCaptchaInput
+                id="captcha"
+                label="FriendlyCaptcha label"
+                name="frc-captcha-response"
+            />
+        </NuxtStoryVariant>
+    </NuxtStory>
+</template>
---
+++ app/components/organisms/VForm/CaptchaTurnstile.stories.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import captchaFieldKey from '~/utils/captcha/providers.constants'
+import type { JsonSchemaExtended } from '~~/types/json-schema'
+
+const config = useRuntimeConfig()
+config.public.turnstile.siteKey = '1x00000000000000000000AA'
+
+const schema = {
+    title: 'test_tous_les_types_de_champs',
+    type: 'object',
+    properties: {
+        text: {
+            type: 'string',
+            title: 'Text',
+        },
+        [captchaFieldKey.TURNSTILE]: {
+            type: 'string',
+            title: 'turnstile captcha',
+            propertyOrder: 15,
+        },
+    },
+    required: [captchaFieldKey.TURNSTILE],
+}
+</script>
+
+<template>
+    <NuxtStory>
+        <VForm :schema="(schema as JsonSchemaExtended)" />
+    </NuxtStory>
+</template>
