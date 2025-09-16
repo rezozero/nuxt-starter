cbd9682a2a4287b16d1f0edd6493b8dfc8505c56
timothe joubert <timothejoubert26@gmail.com>
2025-09-16 14:58:52 +0200

    feat: add VWrapper component

---
+++ app/components/atoms/VWrapper/VWrapper.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { ComponentOrTagName } from '~~/types/app'
+
+/**
+* An HTML tag name or a component used to wrap the output. Output is not
+* wrapped by default.
+*
+* @defaultValue `"template"` (no wrapper)
+*/
+defineProps<{
+    wrapper: ComponentOrTagName | false | undefined
+}>()
+
+defineOptions({
+    inheritAttrs: false,
+})
+</script>
+
+<template>
+    <component
+        :is="wrapper"
+        v-if="wrapper"
+        v-bind="$attrs"
+    >
+        <slot />
+    </component>
+    <template v-else>
+        <slot />
+    </template>
+</template>
