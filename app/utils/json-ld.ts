d2c38264808807ea2a95dae34fd48c9a79a9bf79
Timothe Joubert <35274260+timothejoubert@users.noreply.github.com>
2026-03-24 17:07:33 +0100

    feat: add VBreadcrumb (#439)
    * feat: add breadcrumb components, stories, composable and utils methods
    
    * chore: rename css var
    
    * chore: rename elements
    
    * chore: filter breadcrumbs item with content
    
    * style: keep only essential style
    
    * chore: provide fallback key
    
    * chore: remove possible duplicate homePage item
    
    * chore: prevent duplicate inject of script
    
    * refactor: fix type error on removeNullishNestedKeys
    
    * chore: use-roadiz-breadcrumb return allItems and breadcrumbItems
    
    * don't use callOnce because multiple injected script is allowed
    
    * chore: add type import
    
    * chore: add exemple for eventWebResponse
    
    * chore: simplify ts type
    
    * chore: remove specific project data storie data
    
    * chore: update itemListOrder value
    
    * chore: add implicit import
    
    * chore: update type declaration
    
    * refactor: simplify template and better a11y
    
    * refactor: update ts Type
    
    * refactor: rename breadcrumbItems to items for consistency
    
    * chore: update story name
    
    ---------
    
    Co-authored-by: timothe joubert <timothejoubert26@gmail.com>

---
+++ app/utils/json-ld.ts
@@ -0,0 +1,60 @@
+import type { MaybeRef } from 'vue'
+import { toDisplayString, toValue } from 'vue'
+
+type StructuredDataContent = Record<string, unknown>
+
+const isNullishValue = (v: unknown): boolean => {
+    if (typeof v === 'number') return false
+    if (v === null || v === undefined) return true
+    if (Array.isArray(v)) return v.length === 0
+    if (typeof v === 'object') return Object.keys(v).length === 0
+    return false
+}
+
+export function removeNullishNestedKeys<T extends StructuredDataContent>(obj: T): T {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+        if (isNullishValue(value)) {
+            return acc
+        }
+
+        if (Array.isArray(value)) {
+            const cleanedArr = value
+                .map(v =>
+                    typeof v === 'object' && v !== null
+                        ? removeNullishNestedKeys(v as StructuredDataContent)
+                        : v)
+                .filter(v => !isNullishValue(v))
+
+            if (cleanedArr.length > 0) {
+                (acc as StructuredDataContent)[key] = cleanedArr
+            }
+            return acc
+        }
+
+        if (typeof value === 'object' && value !== null && Object.keys(value).length) {
+            const cleanedObj = removeNullishNestedKeys(value as StructuredDataContent)
+            if (!isNullishValue(cleanedObj)) {
+                (acc as StructuredDataContent)[key] = cleanedObj
+            }
+            return acc
+        }
+
+        (acc as StructuredDataContent)[key] = value
+        return acc
+    }, {} as T)
+}
+export function getJsonLdScriptContent(content: MaybeRef<StructuredDataContent | unknown[]>) {
+    const value = toValue(content)
+
+    const filteredContent = Array.isArray(value)
+        ? value.filter(v => !isNullishValue(v))
+        : removeNullishNestedKeys(value)
+
+    return {
+        type: 'application/ld+json',
+        innerHTML: toDisplayString(filteredContent).replaceAll(
+            /"@id":\s?"\/api\/\.well-known\/genid\/([^"]+)",\s*/gm, // Remove auto genid from structured data. It could lead Google to follow them as links, plus it is useless.
+            '',
+        ),
+    }
+}
