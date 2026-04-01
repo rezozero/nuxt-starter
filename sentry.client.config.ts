956531e7ee38b345ac67e235cf1b9a857695f0e2
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2026-04-01 22:42:43 +0200

    fix: checks if sentry config exists

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nuxt'
 const runtimeConfig = useRuntimeConfig()
 const { sentry, site } = runtimeConfig.public
 
-if (sentry.dsn) {
+if (sentry?.dsn) {
     Sentry.init({
         dsn: sentry.dsn,
         environment: site.env,
