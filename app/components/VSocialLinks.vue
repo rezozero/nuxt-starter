e507ee0c9d4d10daab864913fce254ecc7dc9341
Timothe Joubert <35274260+timothejoubert@users.noreply.github.com>
2026-04-13 16:03:56 +0200

    feat: add social media icons and component for social links (#542)
    Co-authored-by: timothe joubert <timothejoubert26@gmail.com>

---
+++ app/components/VSocialLinks.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const socials = useRoadizHeadSocialLinks()
+</script>
+
+<template>
+    <ul :class="$style.root">
+        <li
+            v-for="social in socials"
+            :key="social.url"
+        >
+            <slot :social="social" />
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" module>
+.root {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    padding: var(--v-social-links-padding, 0);
+    margin: var(--v-social-links-margin, 0);
+    gap: var(--spacing-4xs);
+    list-style: none;
+}
+
+.button {
+    --v-button-padding: 12px;
+    --v-button-font-size: 18px;
+}
+</style>
