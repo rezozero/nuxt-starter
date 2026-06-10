16a7cd91640e179ef6af6a7f751e1bc345269f08
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2026-06-10 14:57:35 +0200

    refactor: create blocks/ folder

---
+++ stories/app/components/blocks/ContentBlock/Default.stories.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { RoadizWalkerKnown } from '~/utils/roadiz/types'
+import type { NSContentBlock } from '~~/types/roadiz'
+
+const walker: RoadizWalkerKnown<NSContentBlock> = {
+    item: {
+        slug: 'content-block',
+        title: 'Hello world',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    children: [],
+}
+</script>
+
+<template>
+    <NuxtStory>
+        <ContentBlock
+            :walker="walker"
+            :index="0"
+        />
+    </NuxtStory>
+</template>
