ceb19ff03c456e3babf8ab23081c4ef9d0e8f781
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2026-07-16 18:32:57 +0200

    fix: simplify copyright component styles and structure

--- stories/app/components/VCopyright/Default.stories.vue
+++ stories/app/components/VCopyright/Default.stories.vue
@@ -2,12 +2,18 @@
     <NuxtStory>
         <NuxtStoryVariant title="Content - prop">
             <div :class="$style.container">
-                <VCopyright content="Lorem ipsum" />
+                <VCopyright
+                    content="Lorem ipsum"
+                    :class="$style.copyright"
+                />
             </div>
         </NuxtStoryVariant>
         <NuxtStoryVariant title="Content - default slot">
             <div :class="$style.container">
-                <VCopyright v-slot="{ id, itemClass, isExpanded }">
+                <VCopyright
+                    v-slot="{ id, itemClass, isExpanded }"
+                    :class="$style.copyright"
+                >
                     <span
                         :id="id"
                         :class="itemClass"
@@ -27,5 +33,11 @@
     width: 200px;
     height: 100px;
     background-color: lightgrey;
+}
+
+.copyright {
+    position: absolute;
+    right: 12px;
+    bottom: 12px;
 }
 </style>
